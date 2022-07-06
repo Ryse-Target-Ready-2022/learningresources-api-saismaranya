19269a99dd96079ee98c0c878f8b888da42079ca
saismaranya <75244150+saismaranya@users.noreply.github.com>
2022-07-06 20:12:47 +0530

    <first commit of java helloworld>

---
+++ learningresources-api/src/main/java/com/tgt/rysetii/learningresourcesapi/LearningresourcesApiApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetii.learningresourcesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
