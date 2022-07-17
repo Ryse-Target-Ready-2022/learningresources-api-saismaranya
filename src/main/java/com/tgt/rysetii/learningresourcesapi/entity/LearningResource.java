08e2947f779fcf9eaa862f984381f641ed04fb3e
saismaranya <75244150+saismaranya@users.noreply.github.com>
2022-07-17 15:33:51 +0530

    jpa

---
+++ src/main/java/com/tgt/rysetii/learningresourcesapi/entity/LearningResource.java
@@ -0,0 +1,113 @@
+package com.tgt.rysetii.learningresourcesapi.entity;
+
+
+import java.io.Serializable;
+import java.util.*;
+
+
+public class LearningResource implements Serializable {
+    private int id;
+    private String name;
+    private double costPrice;
+    private double sellingPrice;
+    private boolean productStatus;
+    private Date createdDate;
+    private Date publishedDate;
+    private Date retiredDate;
+
+    //default constructor
+    public LearningResource() {
+
+    }
+
+    //parameterised constructor
+    public LearningResource(int id, String name, double costPrice, double sellingPrice, boolean productStatus, Date createdDate, Date publishedDate, Date retiredDate) {
+        this.id = id;
+        this.name = name;
+        this.costPrice = costPrice;
+        this.sellingPrice = sellingPrice;
+        this.productStatus = productStatus;
+        this.createdDate = createdDate;
+        this.publishedDate = publishedDate;
+        this.retiredDate = retiredDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(double costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public boolean isProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(boolean productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public Date getRetiredDate() {
+        return retiredDate;
+    }
+
+    public void setRetiredDate(Date retiredDate) {
+        this.retiredDate = retiredDate;
+    }
+
+    @Override
+    public String toString() {
+        return "LearningResource{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", costPrice=" + costPrice +
+                ", sellingPrice=" + sellingPrice +
+                ", productStatus=" + productStatus +
+                ", createdDate=" + createdDate +
+                ", publishedDate=" + publishedDate +
+                ", retiredDate=" + retiredDate +
+                '}';
+    }
+}
+
